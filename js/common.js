f423bfe02c7fa16e5b7bc5dce86fbae684e2978f
Johnny <fznh1989@gmail.com>
2014-01-14 18:05:22 +0800

    JMUI二级导航

--- js/common.js
+++ js/common.js
@@ -1,11 +1,3 @@
 $(function(){
-	//二级导航的显示控制 
-	$('#nav-jmui,#secondNav').mouseover(function(){
-		$('#secondNav').show();
-	});
 
-	$('#nav-jmui,#secondNav').mouseout(function(){
-		$('#secondNav').hide();
-	});
-	
 });
